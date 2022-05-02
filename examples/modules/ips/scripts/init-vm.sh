c0de1c00f39d522329894361db8ed306428d4899
David <david.rolin@gmail.com>
2022-05-02 10:57:13 +0200

    lab 5

---
+++ examples/modules/ips/scripts/init-vm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get -y update
+
+# install Docker
+apt-get -y install docker.io
+
+# install selks in docker
+git clone https://github.com/StamusNetworks/SELKS.git
+cd SELKS/docker/ && ./easy-setup.sh
+cd SELKS/docker/ && docker-compose up -d
